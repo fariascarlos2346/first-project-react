19991147784a85a566b6337264f4bc0fe29abea4
carloshenrique48 <fariascarlos2346@gmail.com>
2023-11-21 12:20:08 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+
+import Familiares from './assets/familiares.png';
+import Arrow from './assets/arrow.svg';
+import Trash from './assets/trash.svg';
+
+import { 
+  Container,
+  H1,
+  Image,
+  ContainerItens,
+  InputLabel,
+  Input,
+  Button,
+  User,
+} from "./styles";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const inputName = useRef()
+  const inputAge = useRef()
+
+  function addNewUser() {
+  //spead operator
+    setUsers([
+      ... users,
+      {
+        id: Math.random(),
+         name: inputName.current.value, 
+         age: inputAge.current.value,
+        },
+      ]);
+  }
+
+  function deleteUser(userId){
+    const newUsers = users.filter(user => user.id !== userId );
+    
+    setUsers(newUsers);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Image alt="logo-image" src={Familiares}/>
+      <ContainerItens>
+        <H1>Olá</H1>
+
+        <InputLabel>Nome</InputLabel>
+        <Input ref={inputName} placeholder="Nome" />
+
+        <InputLabel>Idade</InputLabel>
+        <Input ref={inputAge} placeholder="Idade" />
+
+        <Button onClick={addNewUser}>
+          Cadastrar<img alt="seta" src={Arrow}/>
+        </Button>
+
+        <ul>
+          { users.map( user => (
+            <User key={user.id}>
+              <p>{user.name}</p>  <p>{user.age}</p>
+              <button onClick={() => deleteUser(user.id)}>
+                <img src={Trash} alt="lata-de-lixo"/>
+              </button>
+            </User>
+          ))}
+        </ul>
+
+      </ContainerItens>
+    </Container>
   );
 }
 
